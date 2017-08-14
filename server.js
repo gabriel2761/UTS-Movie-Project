ace6ec5c590f2ec457e42544b5fab2ac818e9e82
Niko Mojica <nikomojica@Nikos-MacBook-Air.local>
2017-08-15 00:19:52 +1000

    Create express with jquery solution. Eventually will replace jquery with react.

--- server.js
+++ server.js
@@ -1,9 +1,43 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 app.use(express.static('public'));
+app.use(express.bodyParser());
+
+var title = 'Gone with the wind';
+var date = '2015-08-10';
+var duration = '85 minutes';
+var genre = 'romance';
+var synopsis = 'Guy marries girl and slaves everywhere';
+
+app.get('/init', (request, response) => {
+	response.send({
+		title: title,
+		date: date,
+		duration: duration,
+		genre: genre,
+		synopsis: synopsis
+	});
+});
+
+app.post('/movie', (request, response) => {
+	title = request.body.title;
+	date = request.body.date;
+	duration = request.body.duration;
+	genre = request.body.genre;
+	synopsis = request.body.synopsis;
+
+	console.log(request.body);
+
+	response.send({
+		title: title,
+		date: date,
+		duration: duration,
+		genre: genre,
+		synopsis: synopsis
+	});
+});
 
 app.listen(7000, () => {
 	console.log('Running Express...');
 });
-
