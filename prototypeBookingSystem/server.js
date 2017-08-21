e9d8b31a8c5f231cc8aed1f8d9849caaa51c299e
Chris Tan <chris.r.tan@outlook.com>
2017-08-21 22:12:34 +1000

    changed bookings to write to mongodb | added package.json for easy module install

--- prototypeBookingSystem/server.js
+++ prototypeBookingSystem/server.js
@@ -3,12 +3,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
 const app = express();
+const MongoClient = require('mongodb').MongoClient, assert = require('assert');
+const mongourl = 'mongodb://localhost:27017/bookingSystem';
 
 app.use(express.static('public'));
 app.use(express.bodyParser());
 app.use(expressValidator()); // this line must be immediately after any of the bodyParser middlewares!
 
-const bookings = [];
+//const bookings = [];
 
 const username = 'user';
 const password = 'password';
@@ -22,19 +24,38 @@ app.post('/login', (request, response) => {
 });
 
 app.get('/bookings', (request, response) => {
-	response.send(bookings);
+  //response.send(bookings);
+
+  MongoClient.connect(mongourl, function(err, db) {
+    assert.equal(null, err);
+    db.collection('bookings').find().toArray(function(err, docs) {
+      assert.equal(null, err);
+      console.log(docs);
+      response.send(docs);
+    });
+    db.close();
+  });
 });
 
 app.post('/book', (request, response) => {
-	bookings.push(request.body);
+  //bookings.push(request.body);
+        
+  MongoClient.connect(mongourl, function(err, db) {
+    assert.equal(null, err);
+    db.collection('bookings').insertOne(request.body,function(err, r) {
+      assert.equal(null, err);
+      assert.equal(1, r.insertedCount);
+    });
+    db.close();
+  });
 
-	response.send({
-	  this: 'happends'
-	});
+  response.send({
+    this: 'happends'
+  });
 });
 
-const port = 7000;
+const port = 3000;
 
 app.listen(port, () => {
-	console.log(`Running Express on port ${port}...`);
+  console.log(`Running Express on port ${port}...`);
 });
