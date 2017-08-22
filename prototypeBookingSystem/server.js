23b3f7e2306d5e072ada0863830825b2899bac18
Chris Tan <chris.r.tan@outlook.com>
2017-08-22 20:05:29 +1000

    improved styling for server.js

--- prototypeBookingSystem/server.js
+++ prototypeBookingSystem/server.js
@@ -5,57 +5,50 @@ const expressValidator = require('express-validator');
 const app = express();
 const MongoClient = require('mongodb').MongoClient, assert = require('assert');
 const mongourl = 'mongodb://localhost:27017/bookingSystem';
+const username = 'user';
+const password = 'password';
+const PORT = 3000;
 
 app.use(express.static('public'));
 app.use(express.bodyParser());
-app.use(expressValidator()); // this line must be immediately after any of the bodyParser middlewares!
+app.use(expressValidator()); // Must be after bodyParser
 
-//const bookings = [];
-
-const username = 'user';
-const password = 'password';
-
+// Rest endpoint to receive login post requests
 app.post('/login', (request, response) => {
-  if (username === request.body.username && password === request.body.password) {
+    if (username === request.body.username && password === request.body.password) {
 	response.send(true);
-  } else {
+    } else {
 	response.send(false);
-  }
+    }
 });
 
+// Rest endpoint to fetch all bookings from database
 app.get('/bookings', (request, response) => {
-  //response.send(bookings);
-
-  MongoClient.connect(mongourl, function(err, db) {
-    assert.equal(null, err);
-    db.collection('bookings').find().toArray(function(err, docs) {
-      assert.equal(null, err);
-      console.log(docs);
-      response.send(docs);
+    MongoClient.connect(mongourl, function(err, db) {
+	assert.equal(null, err);
+	db.collection('bookings').find().toArray(function(err, docs) {
+	    assert.equal(null, err);
+	    console.log(docs);
+	    response.send(docs);
+	});
+	db.close();
     });
-    db.close();
-  });
 });
 
+// Rest endpoint to insert bookings into database
 app.post('/book', (request, response) => {
-  //bookings.push(request.body);
-        
-  MongoClient.connect(mongourl, function(err, db) {
-    assert.equal(null, err);
-    db.collection('bookings').insertOne(request.body,function(err, r) {
-      assert.equal(null, err);
-      assert.equal(1, r.insertedCount);
+    MongoClient.connect(mongourl, function(err, db) {
+	assert.equal(null, err);
+	db.collection('bookings').insertOne(request.body,function(err, r) {
+	assert.equal(null, err);
+	assert.equal(1, r.insertedCount);
+	});
+	db.close();
     });
-    db.close();
-  });
-
-  response.send({
-    this: 'happends'
-  });
+    response.send({this: 'happends'});
 });
 
-const port = 3000;
-
-app.listen(port, () => {
-  console.log(`Running Express on port ${port}...`);
+// Start express server on predefined port
+app.listen(PORT, () => {
+    console.log(`Running Express on port ${PORT}...`);
 });
