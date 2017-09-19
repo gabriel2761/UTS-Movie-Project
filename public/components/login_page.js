57d7afa5a05f31c470d0cd78512f56752907ec95
Chris Tan <chris.r.tan@outlook.com>
2017-09-19 15:32:07 +1000

    implemented login with passportjs | added mongoose for users | added webpack build script | added nodemon start script

--- public/components/login_page.js
+++ public/components/login_page.js
@@ -6,6 +6,13 @@ import React from 'react';
  * component to logged in.
  */
 class LoginPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      signUp: false,
+    };
+  }
+	
   _login(event) {
 	var self = this;
 	event.preventDefault();
@@ -16,10 +23,10 @@ class LoginPage extends React.Component {
 	})
 
 	.then((response) => {
-	  if (response.data) {
+	  if (response.data.success) {
 		self.props.authenticateUser();
 	  } else {
-		alert('Incorrect username or password');
+		alert(response.data.message);
 	  }
 	})
 
@@ -28,16 +35,64 @@ class LoginPage extends React.Component {
 	});
   }
 
+  _signUp(event) {
+    var self = this;
+    event.preventDefault();
+    
+    axios.post('/signup', {
+      username: this.username.value,
+      password: this.password.value,
+    })
+
+    .then((response) => {
+      if (response.data.success) {
+        self.props.authenticateUser();
+      } else {
+        alert(response.data.message);
+      }
+    })
+
+    .catch((error) => {
+      console.log(error);
+    })
+  }
+
+  _showSignup() {
+    this.setState({signUp: true});
+  }
+
+  _showLogin() {
+    this.setState({signUp: false});
+  }
+
   render() {
-	return (
-	  <form onSubmit={this._login.bind(this)}>
-		<input type="text" placeholder="Username" ref={username => this.username = username} />
-		<br />
-		<input type="password" placeholder="Password" ref={password => this.password = password} />
-		<br />
-		<button type="submit">Login</button>
-	  </form>
-	);
+    if (this.state.signUp) {
+      return (
+	<div>
+          <form onSubmit={this._signUp.bind(this)}>
+            <input type="text" placeholder="Username" ref={username => this.username = username} />
+	    <br />
+            <input type="text" placeholder="Password" ref={password => this.password = password} />
+	    <br />
+	    <button type="submit">Signup</button>
+          </form>
+          <button onClick={this._showLogin.bind(this)}>{"Already have an account? Login Here!"}</button>
+        </div>
+      );
+    } else {
+       return (
+        <div>
+          <form onSubmit={this._login.bind(this)}>
+            <input type="text" placeholder="Username" ref={username => this.username = username} />
+	    <br />
+            <input type="text" placeholder="Password" ref={password => this.password = password} />
+	    <br />
+	    <button type="submit">Login</button>
+          </form>
+          <button onClick={this._showSignup.bind(this)}>{"Don't have an account yet? Sign Up Here!"}</button>
+        </div>
+      );   
+    }
   }
 }
 
